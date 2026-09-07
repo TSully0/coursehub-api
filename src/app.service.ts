53e7a9d7a2c02dbbc93e38c9262ed4d21950a34b
TSully0 <toalasully@gmail.com>
2026-09-07 14:37:41 -0500

    Update 01

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'CourseHub API esta en línea';
+    return 'CourseHub API esta en línea.';
   }
 }
