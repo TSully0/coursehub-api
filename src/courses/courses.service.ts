014d39c9e3d4020fdce4ed4f84ca84a127908050
TSully0 <toalasully@gmail.com>
2026-09-08 15:02:58 -0500

    Update 05

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -25,5 +25,37 @@ export class CoursesService {
     findOne(id: number): Course | undefined {
         return this.courses.find((course) => course.id === id);
     }
-}
+    create(input: CreateCourseInput): Course {
+    const course: Course = {
+        id: Math.max(0, ...this.courses.map((item) => item.id)) + 1,
+        title: input.title,
+        level: input.level,
+    };
 
+    this.courses.push(course);
+    return course;
+    
+    }
+
+    update(id: number, input: UpdateCourseInput): Course | undefined {
+        const course = this.findOne(id);
+
+    if (!course) {
+        return undefined;
+    }
+
+    Object.assign(course, input);
+    return course;
+    }
+
+    remove(id: number): Course | undefined {
+        const index = this.courses.findIndex((course) => course.id === id);
+
+    if (index === -1) {
+        return undefined;
+    }
+
+    const [removedCourse] = this.courses.splice(index, 1);
+    return removedCourse;
+    }
+}
